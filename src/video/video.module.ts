340138d885bdd0177e6e31e6406543d75ecc85df
AshedFox <a.mahn@mail.ru>
2023-03-19 03:35:47 +0300

    fix VideoModule imports and exports

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { VideoResolver } from './video.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideoEntity } from './entities/video.entity';
 
 @Module({
-  providers: [VideoResolver, VideoService]
+  imports: [TypeOrmModule.forFeature([VideoEntity])],
+  providers: [VideoResolver, VideoService],
+  exports: [VideoService],
 })
 export class VideoModule {}
