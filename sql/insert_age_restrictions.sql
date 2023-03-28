041a3a10ac262b906af057f013904e68f552b388
AshedFox <a.mahn@mail.ru>
2023-03-29 00:22:52 +0300

    remove outdated sql file for age restrictions insertion

--- sql/insert_age_restrictions.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.age_restrictions (id, name) VALUES (1, 'G');
-INSERT INTO public.age_restrictions (id, name) VALUES (2, 'PG');
-INSERT INTO public.age_restrictions (id, name) VALUES (3, 'PG-13');
-INSERT INTO public.age_restrictions (id, name) VALUES (4, 'R');
-INSERT INTO public.age_restrictions (id, name) VALUES (5, 'NC-17');
