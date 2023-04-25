fb13d7a3a412853a08e9234c17f0d16fb28f515e
AshedFox <a.mahn@mail.ru>
2023-04-25 16:53:32 +0300

    add pubSub module for subscriptions support

---
+++ src/pub-sub/pub-sub.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+
+@Module({
+  providers: [{ provide: 'PUB_SUB', useClass: PubSub }],
+  exports: ['PUB_SUB'],
+})
+export class PubSubModule {}
