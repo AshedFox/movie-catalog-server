fe9c17fb227e21fe2ae2907d98c72b19fb1252d3
AshedFox <a.mahn@mail.ru>
2023-04-25 17:32:35 +0300

    change media type enum values

--- src/utils/enums/media-type.enum.ts
+++ src/utils/enums/media-type.enum.ts
@@ -1,9 +1,9 @@
 import { registerEnumType } from '@nestjs/graphql';
 
 export enum MediaTypeEnum {
-  VIDEO = 'video',
-  IMAGE = 'image',
-  SUBTITLES = 'subtitles',
+  VIDEO = 'VIDEO',
+  IMAGE = 'IMAGE',
+  RAW = 'RAW',
 }
 
 registerEnumType(MediaTypeEnum, {
