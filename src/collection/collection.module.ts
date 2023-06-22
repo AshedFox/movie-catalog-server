b75af5a030791894241714a986fbc2ae19e423d5
AshedFox <a.mahn@mail.ru>
2023-06-23 02:19:48 +0300

    use transaction on collection creation, remove redundant deps

--- src/collection/collection.module.ts
+++ src/collection/collection.module.ts
@@ -1,15 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CollectionService } from './collection.service';
 import { CollectionResolver } from './collection.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollectionEntity } from './entities/collection.entity';
-import { CollectionMovieModule } from '../collection-movie/collection-movie.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([CollectionEntity]),
-    forwardRef(() => CollectionMovieModule),
-  ],
+  imports: [TypeOrmModule.forFeature([CollectionEntity])],
   providers: [CollectionResolver, CollectionService],
   exports: [CollectionService],
 })
--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -1,11 +1,11 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateCollectionInput } from './dto/create-collection.input';
 import { UpdateCollectionInput } from './dto/update-collection.input';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CollectionEntity } from './entities/collection.entity';
-import { CollectionMovieService } from '../collection-movie/collection-movie.service';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { BaseService } from '@common/services/base.service';
+import { CollectionMovieEntity } from '../collection-movie/entities/collection-movie.entity';
+import { CollectionEntity } from './entities/collection.entity';
 
 @Injectable()
 export class CollectionService extends BaseService<
@@ -16,8 +16,8 @@ export class CollectionService extends BaseService<
   constructor(
     @InjectRepository(CollectionEntity)
     private readonly collectionRepository: Repository<CollectionEntity>,
-    @Inject(forwardRef(() => CollectionMovieService))
-    private readonly collectionMovieService: CollectionMovieService,
+    @InjectDataSource()
+    private readonly dataSource: DataSource,
   ) {
     super(collectionRepository);
   }
@@ -25,16 +25,33 @@ export class CollectionService extends BaseService<
   create = async (
     createCollectionInput: CreateCollectionInput,
   ): Promise<CollectionEntity> => {
-    const collection = await this.collectionRepository.save(
-      createCollectionInput,
-    );
-    const { moviesIds } = createCollectionInput;
-    if (moviesIds) {
-      await this.collectionMovieService.createManyForCollection(
-        collection.id,
-        moviesIds,
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const collection = await queryRunner.manager.save(
+        CollectionEntity,
+        createCollectionInput,
       );
+      const { moviesIds } = createCollectionInput;
+
+      if (moviesIds) {
+        collection.moviesConnection = await queryRunner.manager.save(
+          CollectionMovieEntity,
+          moviesIds.map((movieId) => ({
+            collectionId: collection.id,
+            movieId,
+          })),
+        );
+      }
+      await queryRunner.commitTransaction();
+      return collection;
+    } catch {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
     }
-    return collection;
   };
 }
