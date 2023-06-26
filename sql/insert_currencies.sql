028cf04a7fc383890eb3685e546a5baf64b21e7e
AshedFox <a.mahn@mail.ru>
2023-06-27 00:55:52 +0300

    fix: make currencies ids lower case to avoid problems with Stripe

--- sql/insert_currencies.sql
+++ sql/insert_currencies.sql
@@ -1,3 +1,4 @@
-INSERT INTO public.currencies (id, symbol, name) VALUES ('EUR', '€', 'Euro');
-INSERT INTO public.currencies (id, symbol, name) VALUES ('USD', '$', 'US dollar');
-INSERT INTO public.currencies (id, symbol, name) VALUES ('RUB', '₽', 'Russian ruble');
+INSERT INTO public.currencies (id, symbol, name) VALUES ('eur', '€', 'Euro');
+INSERT INTO public.currencies (id, symbol, name) VALUES ('usd', '$', 'US dollar');
+INSERT INTO public.currencies (id, symbol, name) VALUES ('rub', '₽', 'Russian ruble');
+INSERT INTO public.currencies (id, symbol, name) VALUES ('byn', 'Br', 'Belarussian ruble');
