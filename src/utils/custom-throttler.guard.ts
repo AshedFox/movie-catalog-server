a8edf942b2e17e4bc2ce2f70751d31217870f0df
AshedFox <a.mahn@mail.ru>
2023-09-05 02:59:18 +0300

    update throttler to avoid limiting for subscriptions

--- src/utils/custom-throttler.guard.ts
+++ src/utils/custom-throttler.guard.ts
@@ -4,17 +4,35 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
-  getRequestResponse(context: ExecutionContext) {
+  async handleRequest(
+    context: ExecutionContext,
+    limit: number,
+    ttl: number,
+  ): Promise<boolean> {
+    const reqType = context.getType<ContextType | 'graphql'>();
+
+    if (
+      reqType === 'graphql' &&
+      !!GqlExecutionContext.create(context).getContext().extra
+    ) {
+      return true;
+    }
+
+    return super.handleRequest(context, limit, ttl);
+  }
+
+  getRequestResponse(context: ExecutionContext): {
+    req: Record<string, any>;
+    res: Record<string, any>;
+  } {
     const reqType = context.getType<ContextType | 'graphql'>();
     if (reqType === 'graphql') {
       const gqlCtx = GqlExecutionContext.create(context);
       const ctx = gqlCtx.getContext();
       return { req: ctx.req, res: ctx.res };
     } else if (reqType === 'http') {
-      return {
-        req: context.switchToHttp().getRequest(),
-        res: context.switchToHttp().getResponse(),
-      };
+      const http = context.switchToHttp();
+      return { req: http.getRequest(), res: http.getResponse() };
     }
   }
 }
