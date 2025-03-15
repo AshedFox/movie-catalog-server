d83a80a1de75c68da86db60bdc2e4530a5ac7b6d
AshedFox <a.mahn@mail.ru>
2025-03-16 02:46:15 +0300

    rename audio profiles

--- src/utils/enums/audio-profile.enum.ts
+++ src/utils/enums/audio-profile.enum.ts
@@ -1,14 +1,10 @@
 import { registerEnumType } from '@nestjs/graphql';
 
 export enum AudioProfileEnum {
-  PROFILE_4k = 'PROFILE_4k',
-  PROFILE_2k = 'PROFILE_2k',
-  PROFILE_1080p = 'PROFILE_1080p',
-  PROFILE_720p = 'PROFILE_720p',
-  PROFILE_480p = 'PROFILE_480p',
-  PROFILE_360p = 'PROFILE_360p',
-  PROFILE_240p = 'PROFILE_240p',
-  PROFILE_144p = 'PROFILE_144p',
+  ULTRA = 'ULTRA',
+  HIGH = 'HIGH',
+  MEDIUM = 'MEDIUM',
+  LOW = 'LOW',
 }
 
 registerEnumType(AudioProfileEnum, {
